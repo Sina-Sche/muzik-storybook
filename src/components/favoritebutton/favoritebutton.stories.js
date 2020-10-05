555f72dfb27465a86294c0533aace8bb46a9519f
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-05 22:56:26 +0200

    Add Favouritebutton

---
+++ src/components/favoritebutton/favoritebutton.stories.js
@@ -0,0 +1,8 @@
+import "./favoritebutton.css";
+import favoritebuttonText from "./favoritebutton-text.html";
+import favoritebuttonLogo from "./favoritebutton-logo.html";
+
+export default { title: "Components/Button" };
+
+export const withText = () => favoritebuttonText;
+export const withLogo = () => favoritebuttonLogo;

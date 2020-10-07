d89a3f1e5d54994f71801f8b1aed886bedcec307
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-07 09:19:28 +0200

    Add lockscreen.html

--- src/components/settings/settings.stories.js
+++ src/components/settings/settings.stories.js
@@ -1,8 +1,12 @@
 import "./settings.css";
 import display from "./display.html";
 import volume from "./volume.html";
+import headset from "./headset.html";
+import lockscreen from "./lockscreen.html";
 
 export default { title: "Components/Settings" };
 
 export const Display = () => display;
 export const Volume = () => volume;
+export const Headset = () => headset;
+export const Lockscreen = () => lockscreen;

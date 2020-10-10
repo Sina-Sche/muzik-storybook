f6613292c6964cba1154435ab1e2b092fd7bf48f
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-10 18:59:03 +0200

    Edit settings (layout centered)

--- src/components/navbuttons/bottomnav.stories.js
+++ src/components/navbuttons/bottomnav.stories.js
@@ -1,6 +1,8 @@
 import "./bottomnav.css";
 import Bottomnav from "./bottomnav.html";
 
-export default { title: "Components/Bottomnav" };
-
+export default {
+  title: "Components/Bottomnav",
+  parameters: { layout: "centered" },
+};
 export const Main = () => Bottomnav;

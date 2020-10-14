000bb5d99879bac1470eb06f0d5d95094de3753c
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-14 11:47:12 +0200

    Add pause button

--- src/components/tracks/track.js
+++ src/components/tracks/track.js
@@ -1,5 +1,6 @@
 import "./track.css";
 import playActionSrc from "../../assets/playaction.svg";
+import pauseActionSrc from "../../assets/pause.svg"
 import michaelJacksonSrc from "../../assets/michael.png";
 export function createTrackElement(track) {
     const trackElement = document.createElement("div"); 
@@ -18,35 +19,56 @@ export function createTrackElement(track) {
 
 
     const imgElement = document.createElement ("img");
-    imgElement.src = michaelJacksonSrc;
+    imgElement.src = track.imgSrc;
     imgElement.alt = `Image of ${track.artist}`;
     imgElement.className ="track__image";
 
     const buttonElement = document.createElement ("button");
-    buttonElement.className = "action__button";
-
-    const playActionElement = document.createElement ("img");
-    playActionElement.className = "play__element"
+    buttonElement.className = "action__button"; 
+    
+    const playActionElement = document.createElement ("img");   
+    playActionElement.className = "play__element";
     playActionElement.src = playActionSrc;
-
-    const audioElement =new Audio(track.audioSrc);
-
-    trackElement.append(imgElement,trackDescription,buttonElement)
-    buttonElement.append(playActionElement)
-    trackDescription.append(titleElement, artistElement)
-    
    
-    let count = 0;
-    buttonElement.onclick = playPause; 
-    function playPause () {
-      if(count == 0) {
-        count = 1;
-        audioElement.play ();
-            }else {
-              count = 0;
-              audioElement.pause ();
-            }
-          };  
+
+    trackElement.append(imgElement,trackDescription,buttonElement);
+    trackDescription.append(titleElement, artistElement);
+   buttonElement.append(playActionElement);
     
+   let isPlaying = false
+   const audioElement =new Audio(track.audioSrc);
+ 
+   buttonElement.onclick = function (){
+   
+   if (isPlaying) {
+     audioElement.pause ();
+     playActionElement.src = playActionSrc;     
+     playActionElement.alt = "Play Button";
+  }
+    else {
+      audioElement.play ();
+      playActionElement.src = pauseActionSrc;  
+      playActionElement.alt = "Pause Button";
+      }
+      isPlaying = !isPlaying;
+  };
+  
     return trackElement;
   }
+
+
+
+
+
+  
+  // let count = 0;
+  // buttonElement.onclick = playPause; 
+  // function playPause () {
+  //   if(count === 0) {
+  //     count = 1;
+  //     audioElement.play ();
+  //         }else {
+  //           count = 0;
+  //           audioElement.pause ();
+  //         }
+  //       };  
--- src/components/tracks/track.stories.js
+++ src/components/tracks/track.stories.js
@@ -17,13 +17,15 @@ export const billieJean = () => createTrackElement({
 export const dontstopBelievin = () => createTrackElement({
   title: "Don't stop believin", 
   artist: "Journey",
-  imgSrc: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.dL7ecjsTGmmSqZmH1vVotgAAAA%26pid%3DApi&f=1",});
-export const hopelessWanderer = () => createTrackElement({
-  title: "Hopeless Wanderer", 
-  artist: "Mumford and Sons",
-  imgSrc: " "});
+  imgSrc: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.jXfKR0Wp1X9cEsHhxyQYiQAAAA%26pid%3DApi&f=1",
+  audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_12_-_Annalise.mp3",});
+  export const hopelessWanderer = () => createTrackElement({
+    title: "Hopeless Wanderer", 
+    artist: "Mumford and Sons",
+    imgSrc: "http://img2-ak.lst.fm/i/u/174s/7142e584624346a1b3d8bb045ac0a7b6.png",
+    audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Scott_Holmes/Inspiring__Upbeat_Music/Scott_Holmes_-_05_-_Little_Idea.mp3",});
 export const herecomestheSun = () => createTrackElement({
   title:"Here comes the Sun", 
   artist: "The Beatles",
-  imgSrc: "",
-  audioSrc: ""});
+  imgSrc: "http://www.delta.ro/beatles/gif/sglbeat160x154.gif",
+  audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/KieLoKaz/Free_Ganymed/KieLoKaz_-_01_-_Reunion_of_the_Spaceducks_Kielokaz_ID_365.mp3",});

3cf113ecb9b9ae27a990ff1a1b7619ac02a79851
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-15 14:52:19 +0200

    Add track element

--- src/components/tracks/track.js
+++ src/components/tracks/track.js
@@ -1,56 +1,101 @@
 import "./track.css";
 import playActionSrc from "../../assets/playaction.svg";
 import pauseActionSrc from "../../assets/pause.svg"
+import {createElement} from "../../utils/elements"; 
 
-export function createTrackElement(track) {
-    const trackElement = document.createElement("div"); 
-    trackElement.className = "track";
+export const createTrackElement = (track) => {
+  const audioElement = new Audio (track.audioSrc);
+  const playActionElement = createElement ("img", {
+    src: playActionSrc,
+  });
 
-    const trackDescription = document.createElement ("div");
-    trackDescription.className = "track__description";
+  const trackElement = createElement("div", {
+    className: "track",
+    children: [
+    createElement("img", {
+      className: "track__image",
+      src: track.imgSrc,
+      alt: `Image of ${track.artist}`,
+    }),
+    createElement("div", {
+      className: "track__description",
+      children: [
+        createElement("h3", {
+          innerText: track.title,
+        }),
+        createElement("p", {
+          innerText: track.artist,
+        }),
+      ],
+    }),
+    createElement("button", {
+      className: "track__button", 
+      children: [playActionElement],
+      onclick: () => {
+        if (!audioElement.paused) {
+          audioElement.pause();
+          showPlayIcon(playActionElement);
+        } else {
+          audioElement.play();
+          showPauseIcon(playActionElement);
+        }
+          },
+      }),
+    ],
+    });
+
+  return trackElement;
+  
+
+
+//     const trackElement = document.createElement("div"); 
+    // trackElement.className = "track";
+
+  //   const trackDescription = document.createElement ("div");
+  //   trackDescription.className = "track__description";
       
-    const titleElement = document.createElement("h3"); 
-    titleElement.innerText = track.title; 
-    titleElement.className = "titleName";
+  //   const titleElement = document.createElement("h3"); 
+  //   titleElement.innerText = track.title; 
+  //   titleElement.className = "titleName";
 
-    const artistElement = document.createElement("p");
-    artistElement.innerText = track.artist; 
-    artistElement.className = "artistName"; 
+  //   const artistElement = document.createElement("p");
+  //   artistElement.innerText = track.artist; 
+  //   artistElement.className = "artistName"; 
 
 
-    const imgElement = document.createElement ("img");
-    imgElement.src = track.imgSrc;
-    imgElement.alt = `Image of ${track.artist}`;
-    imgElement.className ="track__image";
+  //   const imgElement = document.createElement ("img");
+  //   imgElement.src = track.imgSrc;
+  //   imgElement.alt = `Image of ${track.artist}`;
+  //   imgElement.className ="track__image";
 
-    const buttonElement = document.createElement ("button");
-    buttonElement.className = "action__button"; 
+  //   const buttonElement = document.createElement ("button");
+  //   buttonElement.className = "action__button"; 
     
-    const playActionElement = document.createElement ("img");   
-    playActionElement.className = "play__element";
-    playActionElement.src = playActionSrc;
+  //   const playActionElement = document.createElement ("img");   
+  //   playActionElement.className = "play__element";
+  //   playActionElement.src = playActionSrc;
      
-    trackElement.append(imgElement,trackDescription,buttonElement);
-    trackDescription.append(titleElement, artistElement);
-    buttonElement.append(playActionElement);
+  //   trackElement.append(imgElement,trackDescription,buttonElement);
+  //   trackDescription.append(titleElement, artistElement);
+  //   buttonElement.append(playActionElement);
 
      
-   const audioElement = new Audio(track.audioSrc);
+  //  const audioElement = new Audio(track.audioSrc);
  
-   buttonElement.onclick = function () {   
-   if (!audioElement.paused) {
-     audioElement.pause();
-     showPlayIcon(playActionElement);    
-  }
-    else {
-      audioElement.play();
-      showPauseIcon(playActionElement);
-    }
-   };
+  //  buttonElement.onclick = function () {   
+  //  if (!audioElement.paused) {
+  //    audioElement.pause();
+  //    showPlayIcon(playActionElement);    
+  // }
+  //   else {
+  //     audioElement.play();
+  //     showPauseIcon(playActionElement);
+  //   }
+  //  };
   
   
-    return trackElement;
-  }
+  //   return trackElement;
+ }
   const showPlayIcon = (element) => {
     element.src = playActionSrc;    
     element.alt = "Play Icon";
@@ -60,18 +105,3 @@ export function createTrackElement(track) {
     element.alt = "Pause Icon";
   };
 
-
-
-
-  
-  // let count = 0;
-  // buttonElement.onclick = playPause; 
-  // function playPause () {
-  //   if(count === 0) {
-  //     count = 1;
-  //     audioElement.play ();
-  //         }else {
-  //           count = 0;
-  //           audioElement.pause ();
-  //         }
-  //       };  
--- src/components/tracks/track.stories.js
+++ src/components/tracks/track.stories.js
@@ -6,27 +6,27 @@ export default {
   parameters: { layout: "centered" },
 };
 
-// export const billieJean = () => createTrackElement({
-//   title: "Billie Jean", 
-//   artist: "Michael Jackson",
-//   imgSrc: michaelJacksonSrc,
-//   audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Algorithms.mp3"
-// ,});
-// export const dontstopBelievin = () => createTrackElement({
-//   title: "Don't stop believin", 
-//   artist: "Journey",
-//   imgSrc: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.jXfKR0Wp1X9cEsHhxyQYiQAAAA%26pid%3DApi&f=1",
-//   audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_12_-_Annalise.mp3",});
-//   export const hopelessWanderer = () => createTrackElement({
-//     title: "Hopeless Wanderer", 
-//     artist: "Mumford and Sons",
-//     imgSrc: "http://img2-ak.lst.fm/i/u/174s/7142e584624346a1b3d8bb045ac0a7b6.png",
-//     audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Scott_Holmes/Inspiring__Upbeat_Music/Scott_Holmes_-_05_-_Little_Idea.mp3",});
-// export const herecomestheSun = () => createTrackElement({
-//   title:"Here comes the Sun", 
-//   artist: "The Beatles",
-//   imgSrc: "http://www.delta.ro/beatles/gif/sglbeat160x154.gif",
-//   audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/KieLoKaz/Free_Ganymed/KieLoKaz_-_01_-_Reunion_of_the_Spaceducks_Kielokaz_ID_365.mp3",});
+export const billieJean = () => createTrackElement({
+  title: "Billie Jean", 
+  artist: "Michael Jackson",
+  imgSrc: michaelJacksonSrc,
+  audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Algorithms.mp3"
+,});
+export const dontstopBelievin = () => createTrackElement({
+  title: "Don't stop believin", 
+  artist: "Journey",
+  imgSrc: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.jXfKR0Wp1X9cEsHhxyQYiQAAAA%26pid%3DApi&f=1",
+  audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_12_-_Annalise.mp3",});
+  export const hopelessWanderer = () => createTrackElement({
+    title: "Hopeless Wanderer", 
+    artist: "Mumford and Sons",
+    imgSrc: "http://img2-ak.lst.fm/i/u/174s/7142e584624346a1b3d8bb045ac0a7b6.png",
+    audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Scott_Holmes/Inspiring__Upbeat_Music/Scott_Holmes_-_05_-_Little_Idea.mp3",});
+export const herecomestheSun = () => createTrackElement({
+  title:"Here comes the Sun", 
+  artist: "The Beatles",
+  imgSrc: "http://www.delta.ro/beatles/gif/sglbeat160x154.gif",
+  audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/KieLoKaz/Free_Ganymed/KieLoKaz_-_01_-_Reunion_of_the_Spaceducks_Kielokaz_ID_365.mp3",});
 
   export const songs = () => {
     const BillieJeanTrack = {title: "Billie Jean", 
@@ -41,9 +41,14 @@ export default {
     artist: "Mumford and Sons",
     imgSrc: "http://img2-ak.lst.fm/i/u/174s/7142e584624346a1b3d8bb045ac0a7b6.png",
     audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Scott_Holmes/Inspiring__Upbeat_Music/Scott_Holmes_-_05_-_Little_Idea.mp3",}
+    const herecomestheSunTrack = {title:"Here comes the Sun", 
+      artist: "The Beatles",
+      imgSrc: "http://www.delta.ro/beatles/gif/sglbeat160x154.gif",
+      audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/KieLoKaz/Free_Ganymed/KieLoKaz_-_01_-_Reunion_of_the_Spaceducks_Kielokaz_ID_365.mp3",}
 
-    const container = document.createElement('div');
-    const playlist = [BillieJeanTrack, dontstopBelievinTrack, hopelessWandererTrack]
+      const playlist = [BillieJeanTrack, dontstopBelievinTrack, hopelessWandererTrack, herecomestheSunTrack]
+      
+    const container = document.createElement("div");
     playlist.forEach((track) => {
       const trackElement = createTrackElement(track);
       container.append(trackElement);
